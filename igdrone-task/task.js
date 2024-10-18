cfd923755131a8cbea0b1520f37b0a9f726cadf5
simplebarsha <167398434+simplebarsha@users.noreply.github.com>
2024-10-18 07:48:13 +0530

    my-branch

--- igdrone-task/task.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const nameElement = document.querySelector('.name-designation strong');
-    const designationElement = document.querySelector('.name-designation p');
-
-    // You can dynamically set these values
-    nameElement.textContent = "John Doe";
-    designationElement.textContent = "(Manager)";
-});
--- igdrone-task/task2.js
+++
@@ -1,6 +0,0 @@
-<script>
-  document.querySelector('.name').textContent = "Sarat Kumar Naik";
-  document.querySelector('.designation').textContent = "Graphics Designer";
-  document.querySelector('.profile-img').src = "path/to/profile-image.jpg";
-</script>
-
